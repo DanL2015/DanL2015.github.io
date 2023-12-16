421c414b1f7179992db39747e4ae4f2c978535ae
Daniel Liu <daniel17liu@gmail.com>
2023-12-16 11:38:18 -0800

    modified posts

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
 import Right from "./Right";
-import posts from "./Posts.json";
+import json from "./Posts.json";
 import Post from "./Post";
 import useLocalStorageState from "use-local-storage-state";
 
 function Blog() {
   const [theme, setTheme] = useLocalStorageState("theme", "Dark");
-  posts = JSON.parse(JSON.stringify(posts));
+  let posts = JSON.parse(JSON.stringify(json));
   return (
     <div className="background" data-theme = {theme}>
       <Navbar curPage="Blog" />

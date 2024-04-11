bee8b085b7f5be21408609bc6671426e55c566b6
Daniel Liu <daniel17liu@gmail.com>
2024-04-10 18:48:16 -0700

    removed more stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/website',
-  images: {
-    unoptimized: true,
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
